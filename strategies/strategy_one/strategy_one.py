5edced40ae849401f6fa6a1c5672eb0cef672a22
yashgoya9898 <yashgoyal9898@gmail.com>
2025-10-14 10:48:33 +0530

    update state after order

--- strategies/strategy_one/strategy_one.py
+++ strategies/strategy_one/strategy_one.py
@@ -60,6 +60,13 @@ class StrategyOne(BaseStrategy):
             self.ws_mgr.subscribe_symbol("NSE:NIFTY25OCT24800CE", mode="tick")
             logger.info(f"[{self.strategy_id}] Position OPEN: {active_symbol}, Qty: {net_qty}")
 
+
+    async def update_state_after_order(self, active_order_id):
+        logger.info(f"[{self.strategy_id}] Order placed with ID: {active_order_id}")
+        self.trades_done += 1
+        active_trade_data_obj = await self.order_state_manager.add_trade(self.fyers_order_placement, self.active_order_id)
+        self.active_trade_data_obj: Optional[TradeData] = active_trade_data_obj
+
     # ------------------ Consumers ------------------
     async def candle_consumer(self):
         # Skip candle
@@ -74,12 +81,9 @@ class StrategyOne(BaseStrategy):
                 condition_met = await self.strategy_logic_manager.check_entry_condition(self.strategy_id, candle.symbol, candle)
                 if condition_met:
                     order_response = await self.fyers_order_placement.place_order(symbol="NSE:IDEA-EQ", qty=1, order_type=2, side=1, stop_loss=0.5, take_profit=2.0)
-                    self.active_order_id = order_response.get("id")
-                    if self.active_order_id and order_response.get('code') == 1101:
-                        logger.info(f"[{self.strategy_id}] Order placed with ID: {self.active_order_id}")
-                        self.trades_done += 1
-                        active_trade_data_obj = await self.order_state_manager.add_trade(self.fyers_order_placement, self.active_order_id)
-                        self.active_trade_data_obj: Optional[TradeData] = active_trade_data_obj
+                    if order_response.get('code') == 1101:
+                        self.active_order_id = order_response.get("id")
+                        await self.update_state_after_order(self.active_order_id)
 
     async def tick_consumer(self):
         while True:
